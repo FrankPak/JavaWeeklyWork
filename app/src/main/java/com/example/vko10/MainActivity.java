20ba2d6012e3975d15e7dc862f7762aac78bf085
franc <francois.pakkanen@gmail.com>
2022-04-01 17:37:25 +0300

    JavaWeek10

---
+++ app/src/main/java/com/example/vko10/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.vko10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView web;
+    Button search;
+    Button foward;
+    Button back;
+    Button refresh;
+    EditText url;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web = findViewById(R.id.webView);
+        web.setWebViewClient(new WebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
+
+        //web.loadUrl("file:///android_asset/index.html");
+        search = findViewById(R.id.seachbtn);
+        foward = findViewById(R.id.fowardbtn);
+        back = findViewById(R.id.backbtn);
+        refresh = findViewById(R.id.refreshbtn);
+        url = findViewById(R.id.urlText);
+
+    }
+    public void initializeJavascript(View v){
+        String currentSiteUrl = web.getUrl();
+        String htmlUrl = "file:///android_asset/index.html";
+
+        if(!currentSiteUrl.equals(htmlUrl)){
+            web.loadUrl("file:///android_asset/index.html");
+        }
+        web.evaluateJavascript("javascript:initialize()", null);
+    }
+    public void shoutoutJavascript(View v){
+        String currentSiteUrl = web.getUrl();
+        String htmlUrl = "file:///android_asset/index.html";
+
+        if(!currentSiteUrl.equals(htmlUrl)){
+            web.loadUrl("file:///android_asset/index.html");
+        }
+        web.evaluateJavascript("javascript:shoutOut()", null);
+
+    }
+    public void siterefresh(View v){
+        web.reload();
+    }
+    public void searchUrl(View v){
+        String textUrl = url.getText().toString();
+
+        web.loadUrl("https://"+textUrl);
+    }
+}
